85f867e00f4abc3bf622932cb85d7da0b74eb582
YanSchw <schoessow.yannick@gmail.com>
2026-01-03 22:47:31 +0100

    cleanup

--- ShaderCode/include/Constants.glsl
+++ ShaderCode/include/Constants.glsl
@@ -1,5 +1,9 @@
 // ---------- Constants ----------
 const int MAX_BOUNCES = 4;
-const float EPSILON = 1e-4;
-const float PI = 3.14159265359;
-const float INFINITY = 1e30;
+const float EPSILON = 1e-6;
+const float SPLT_EPS = 4 * EPSILON;
+const float LGT_EPS = 5e-5;
+const float REFR_EPS = 1e-4f;
+const float NORM_EPS = 1E-12f;
+const float INFINITY = 1e500;
+const float PI = 3.1415926535897932384;
