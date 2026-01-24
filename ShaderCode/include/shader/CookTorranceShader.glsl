b7cc36467bdbb842296d1bc0af642adced1e6eee
YanSchw <schoessow.yannick@gmail.com>
2026-01-24 15:10:35 +0100

    fixed cook torrance again

--- ShaderCode/include/shader/CookTorranceShader.glsl
+++ ShaderCode/include/shader/CookTorranceShader.glsl
@@ -32,22 +32,61 @@ vec3 shadeCookTorranceShaderGI(inout Ray ray, inout vec3 throughput) {
     const vec3 diffuseColor = shader.diffuseColor_f0.xyz;
     const vec3 ctColor = shader.ctColor_m.xyz;
     const float f0 = shader.diffuseColor_f0.w;
-    const float m = shader.ctColor_m.w;
+    const float m = max(shader.ctColor_m.w, 0.0);
 
     vec3 fragmentColor = vec3(0);
 
-    if (m >= 0) {
-        Illumination illum = illuminate(ray, getRandomLight());
+    Illumination illum = illuminate(ray, getRandomLight());
+
+    const float NdotL = max(0, dot(-illum.direction, ray.normal));
+    if (NdotL <= 0) {
+        ray = shadeIndirectLight(ray, diffuseColor, throughput);
+        return fragmentColor;
+    }
+
+    // Diffuse term
+    const vec3 diffuse = diffuseColor / PI;
+    fragmentColor += diffuse * NdotL * illum.color * lightCount;
+
+    // Cook-Torrance term
+    // half angle vector
+    const vec3 H = normalize(-illum.direction - ray.direction);
+    const float NdotH = max(0, dot(ray.normal, H));
+    const float NdotV = max(0, dot(ray.normal, -ray.direction));
+    const float VdotH = max(0, dot(-ray.direction, H));
+
+    if (NdotV * NdotL > EPSILON) {
+        const vec3 specular = ctColor * (F(VdotH, f0) * D(NdotH, m) * G(NdotH, NdotV, VdotH, NdotL)) / (PI * NdotV * NdotL);
+
+        fragmentColor += specular * NdotL * illum.color * lightCount;
+    }
+    
+    ray = shadeIndirectLight(ray, diffuseColor, throughput);
+
+    return fragmentColor;
+}
+
+vec3 shadeCookTorranceShader(inout Ray ray, in vec3 throughput) {
+    const CookTorranceShader shader = cookTorranceShaders[ray.primitive.shaderIndex];
+    const vec3 diffuseColor = shader.diffuseColor_f0.xyz;
+    const vec3 ctColor = shader.ctColor_m.xyz;
+    const float f0 = shader.diffuseColor_f0.w;
+    const float m = max(shader.ctColor_m.w, 0.0);
+    ray.remainingBounces = 0;
+
+    vec3 fragmentColor = vec3(0);
+
+    // Accumulate the light over all light sources
+    for (int i = 0; i < lightCount; i++) {
+        Illumination illum = illuminate(ray, lights[i]);
 
         const float NdotL = max(0, dot(-illum.direction, ray.normal));
-        if (NdotL <= 0) {
-            ray = shadeIndirectLight(ray, diffuseColor, throughput);
-            return fragmentColor;
-        }
+        if (NdotL <= 0)
+            continue;
 
         // Diffuse term
         const vec3 diffuse = diffuseColor / PI;
-        fragmentColor += diffuse * NdotL * illum.color * lightCount;
+        fragmentColor += diffuse * NdotL * illum.color;
 
         // Cook-Torrance term
         // half angle vector
@@ -59,53 +98,9 @@ vec3 shadeCookTorranceShaderGI(inout Ray ray, inout vec3 throughput) {
         if (NdotV * NdotL > EPSILON) {
             const vec3 specular = ctColor * (F(VdotH, f0) * D(NdotH, m) * G(NdotH, NdotV, VdotH, NdotL)) / (PI * NdotV * NdotL);
 
-            fragmentColor += specular * NdotL * illum.color * lightCount;
-        }
-        
-        ray = shadeIndirectLight(ray, diffuseColor, throughput);
-    } else {
-        ray.remainingBounces = 0;
-    }
-
-    return fragmentColor;
-}
-
-vec3 shadeCookTorranceShader(inout Ray ray, in vec3 throughput) {
-    const CookTorranceShader shader = cookTorranceShaders[ray.primitive.shaderIndex];
-    const vec3 diffuseColor = shader.diffuseColor_f0.xyz;
-    const vec3 ctColor = shader.ctColor_m.xyz;
-    const float f0 = shader.diffuseColor_f0.w;
-    const float m = shader.ctColor_m.w;
-    ray.remainingBounces = 0;
-
-    vec3 fragmentColor = vec3(0);
-
-    if (m >= 0) {
-        // Accumulate the light over all light sources
-        for (int i = 0; i < lightCount; i++) {
-            Illumination illum = illuminate(ray, lights[i]);
-
-            const float NdotL = max(0, dot(-illum.direction, ray.normal));
-            if (NdotL <= 0)
-                continue;
-
-            // Diffuse term
-            const vec3 diffuse = diffuseColor / PI;
-            fragmentColor += diffuse * NdotL * illum.color;
-
-            // Cook-Torrance term
-            // half angle vector
-            const vec3 H = normalize(-illum.direction - ray.direction);
-            const float NdotH = max(0, dot(ray.normal, H));
-            const float NdotV = max(0, dot(ray.normal, -ray.direction));
-            const float VdotH = max(0, dot(-ray.direction, H));
-
-            if (NdotV * NdotL > EPSILON) {
-                const vec3 specular = ctColor * (F(VdotH, f0) * D(NdotH, m) * G(NdotH, NdotV, VdotH, NdotL)) / (PI * NdotV * NdotL);
-
-                fragmentColor += specular * NdotL * illum.color;
-            }
+            fragmentColor += specular * NdotL * illum.color;
         }
     }
+    
     return fragmentColor;
 }
